b094cc88ec23f35ff252e0e012febd645440592f
codyjogg <coty1055@gmail.com>
2026-05-06 19:10:59 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+name = input("What is the name?" )
+time = input("What time did the incident take place?" )
+location = input("Where did this incident take place?" )
+event = input("Please describe the incident." )
+medical = input("Was medical involved? Yes/No" )
+injury = input("Is there an injury? If so please describe in great detail where the injury is located and the extent of the injury." )
+
+print(f"{name}, the time this incident happened was {time}, the location was {location},  what took place {event}, was medical involved? {medical}, and what injury was reported {injury}" )
